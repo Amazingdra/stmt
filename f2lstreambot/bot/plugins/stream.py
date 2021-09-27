5425d88586f3abf97d26bf08dcad8c15b9d498cd
ramesh25022001 <61628444+ramesh25022001@users.noreply.github.com>
2021-09-28 02:03:23 +0530

    Update stream.py

--- f2lstreambot/bot/plugins/stream.py
+++ f2lstreambot/bot/plugins/stream.py
@@ -69,10 +69,11 @@ async def media_receive_handler(_, m: Message):
     sl = Var.URL + str(log_msg.message_id)
     fln = log_msg.video.file_name
     out = f"https://link.playdisk.xyz/st?api=1cb40bcf2199038e39da5fe338a1f00f71c0911c&url={sl}"
+    playit = f"https://playit.playdisk.xyz/st?api=2b6a764deb2bac2525c7b7764e8793e15baa516a&url={sl}"
     await m.reply_text(
-        text=f"**File Name:** `{fln}`\n\n**Streaming Link:** {out}\n\n`©️Powered by` @playdisk_xyz",
+        text=f"**File Name:** {fln}\n\n**Streaming / Download Link:** {out}\n\n©️Powered by @playdisk_xyz \n\n playit link 👇",
         quote=True,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Stream/download Now🤗', url=out)]])
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('playit link🤗', url=playit)]])
     )
 
 @PdiskBot.on_message(filters.channel & (filters.document | filters.video))

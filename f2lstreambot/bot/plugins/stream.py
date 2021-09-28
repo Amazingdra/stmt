6a0ad1e490f748f000ca08445fc17dba39ac5229
ramesh25022001 <61628444+ramesh25022001@users.noreply.github.com>
2021-09-28 10:25:27 +0530

    Update stream.py

--- f2lstreambot/bot/plugins/stream.py
+++ f2lstreambot/bot/plugins/stream.py
@@ -71,7 +71,7 @@ async def media_receive_handler(_, m: Message):
     out = f"https://link.playdisk.xyz/st?api=1cb40bcf2199038e39da5fe338a1f00f71c0911c&url={sl}"
     playit = f"https://playit.playdisk.xyz/st?api=2b6a764deb2bac2525c7b7764e8793e15baa516a&url={sl}"
     await m.reply_text(
-        text=f"**File Name:** {fln}\n\n**Streaming / Download Link:** {out}\n\n©️Powered by @playdisk_xyz \n\n playit link 👇",
+        text=f"**File Name:** {fln}\n\n**Streaming / Direct Download Link👇🏿** {out}\n\n©️Powered by @playdisk_xyz \n\n playit link 👇",
         quote=True,
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('playit link🤗', url=playit)]])
     )

6607b0c7fbb0e25cbcb9092dc9d32b91aeca14f7
ramesh25022001 <61628444+ramesh25022001@users.noreply.github.com>
2022-05-06 12:25:32 +0530

    Update stream.py

--- f2lstreambot/bot/plugins/stream.py
+++ f2lstreambot/bot/plugins/stream.py
@@ -39,8 +39,8 @@ async def media_receive_handler(_, m: Message):
     sl = Var.URL + str(log_msg.message_id)
     fln = log_msg.document.file_name
     name = urllib.parse.quote(fln)
-    out = f"https://stream.shorturllink.in/st?api=af5e38dfaf8b900b45335173d279b44d7ae4b2e9&url={sl}/{name}"
-    playit = f"https://blog.textpage.xyz/surprising-physical-oddity-guys-look-for-in-women/"
+    out = f"https://stream.url2go.in//st?api=af5e38dfaf8b900b45335173d279b44d7ae4b2e9&url={sl}/{name}"
+    playit = f"google.com"
     await m.reply_text(
         text=f"**File Name:** {fln}\n\n**Streaming / Download Link:** {out}\n\n©️Powered by @A2z_tech",
         quote=True,
@@ -72,8 +72,8 @@ async def media_receive_handler(_, m: Message):
     sl = Var.URL + str(log_msg.message_id)
     fln = log_msg.video.file_name
     name = urllib.parse.quote(fln)
-    out = f"https://stream.shorturllink.in/st?api=af5e38dfaf8b900b45335173d279b44d7ae4b2e9&url={sl}/{name}"
-    playit = f"https://blog.textpage.xyz/surprising-physical-oddity-guys-look-for-in-women/"
+    out = f"https://stream.url2go.in//st?api=af5e38dfaf8b900b45335173d279b44d7ae4b2e9&url={sl}/{name}"
+    playit = f"google.com"
     await m.reply_text(
         text=f"**File Name:** {fln}\n\n**Streaming / Direct Download Link👇🏿** {out}\n\n©️Powered by @A2z_tech",
         quote=True,
